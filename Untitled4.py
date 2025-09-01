44e9c4fa42bb41ee23e4f96b63e3628cabf5cbfb
Kyukkj <ea.kahrel@gmail.com>
2025-09-01 15:15:53 -0300

    Criado usando o Colab

---
+++ Untitled4.py
@@ -0,0 +1,57 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPBzyN/UE4jPSNnUvP5qhe4",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Kyukkj/PojetoT3ste-/blob/main/Untitled4.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 2,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "wAgmvOt_c_Cd",
+        "outputId": "1a1c2bc4-e7ec-4a64-e4ce-72809240373e"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Digite o nome da pessoa: asdas\n",
+            "Digite sua idade: d\n",
+            "Nome asdas Idade d\n"
+          ]
+        }
+      ],
+      "source": [
+        "nome = input(\"Digite o nome da pessoa: dad\")\n",
+        "idade = input(\"Digite sua idade: \")\n",
+        "print(\"Nome\", nome,\"Idade\", idade)"
+      ]
+    }
+  ]
+}
